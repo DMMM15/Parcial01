03061c95add1ac39fafedee2ad7e630892115888
DMMM15 <mateomoyano1517@gmail.com>
2025-09-06 00:04:33 +0000

    se resolvieron los errores de los codigos

--- parcial-2/oop.py
+++ parcial-2/oop.py
@@ -5,11 +5,14 @@ class Animal:
     def __init__(self, especie):
         self.especie = especie
     def hablar(self):
-        print('Hace un sonido')
+        print(f'{self.especie} Hace un sonido')
 
 class Perro(Animal):
     def hablar(self):
-        print('Guau!')
+        print(f' {self.especie} Guau!')
 
-p = Perro('Canino')
-p.hablar
+if __name__ == "__main__":
+  animal = Animal ("bulldog")
+  animal.hablar()
+  animal = Perro("bulldog")
+  animal.hablar()
